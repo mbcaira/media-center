c2ae8ca60235fc30be07d547c51eecf2d1e5a08d
Michael Caira <mbcaira@gmail.com>
2022-11-14 21:18:44 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------
+ * CMSIS-RTOS 'main' function template
+ *---------------------------------------------------------------------------*/
+
+#define osObjectsPublic                     // define objects in main module
+#include "osObjects.h"                      // RTOS object definitions
+#include "GLCD.h"
+#include "Board_LED.h"
+#include "Board_Joystick.h"
+#include "Board_ADC.h"
+
+const int debug = 0;
+const unsigned short FI = 1;
+
+extern int Menu_Start(void);
+
+/*
+ * main: initialize and start the system
+ */
+int main (void) {
+  osKernelInitialize ();                    // initialize CMSIS-RTOS
+
+  // initialize peripherals here
+	if (!debug) {
+		GLCD_Init();
+		GLCD_Clear(White);                         /* Clear graphical LCD display   */
+		GLCD_SetBackColor(White);
+		GLCD_SetTextColor(Blue);
+		GLCD_DisplayString(0, 0, FI, (unsigned char*)"     COE718 Project     ");
+		
+		LED_Initialize();
+		Joystick_Initialize();
+	}
+	
+	Menu_Start();
+	
+  osKernelStart();
+}
